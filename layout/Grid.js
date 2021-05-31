3dfb015433c5b9b353dd0620bf23e8e88a1a2d3f
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-05-31 11:56:11 -0700

    adjusted unresponsive card grid columns

--- layout/Grid.js
+++ layout/Grid.js
@@ -2,7 +2,7 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 export default function Grid({ children }) {
   return (
-    <SimpleGrid minChildWidth={["400px"]} columns={2} spacing="10">
+    <SimpleGrid columns={[1, 2]} spacing="10">
       {children}
     </SimpleGrid>
   );
