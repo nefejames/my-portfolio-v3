ae55fc2b3d6ba10a19401f3d1ffa6d56b69b547c
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-09-30 00:46:37 -0700

    added color to blog title

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
 import Link from "next/link";
 import { Card } from "./index";
 
@@ -8,12 +8,18 @@ export default function BlogCard() {
       <a>
         <Card CardForBlog>
           <Box p={5}>
-            <Heading pb={2}>Blog Title</Heading>
+            <Heading pb={2} color="#00DDFF">
+              Blog Title
+            </Heading>
             <Text fontSize={["lg", "xl"]}>
               A webshop with focus on storytelling and the high quality products
               created by Andersen-Andersen, a Danish work-wear clothing
               manufacturer.
             </Text>
+
+            <Button mt="5" color="black" bg="#00DDFF" borderRadius="none">
+              Read more
+            </Button>
           </Box>
         </Card>
       </a>
