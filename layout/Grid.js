8a7da443c4c1d53b83fe7d8a5a4ae4f1e8d3b955
nefejames <nefejames@yahoo.com>
2022-01-22 17:44:32 +0000

    adjusted grid columns

--- layout/Grid.js
+++ layout/Grid.js
@@ -2,7 +2,7 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 export default function Grid({ children }) {
   return (
-    <SimpleGrid columns={[1, 2]} spacing="10">
+    <SimpleGrid columns={[1, null, 2]} spacing="10">
       {children}
     </SimpleGrid>
   );
