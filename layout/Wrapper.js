2377cb96c237b22e3d28698ae815f09f0249b143
nefejames <nefejames@yahoo.com>
2022-01-22 17:44:54 +0000

    fixed padding

--- layout/Wrapper.js
+++ layout/Wrapper.js
@@ -3,7 +3,7 @@ import { Box, Center } from "@chakra-ui/react";
 export default function Wrapper({ children }) {
   return (
     <Center>
-      <Box px={[5, 0]} py={[10, 20]} maxWidth={"1000px"}>
+      <Box px={[5, 10]} py={[10, 20]} maxWidth={"1000px"}>
         {children}
       </Box>
     </Center>
